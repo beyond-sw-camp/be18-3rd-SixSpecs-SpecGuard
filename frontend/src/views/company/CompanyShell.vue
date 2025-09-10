f13e31316c2cf6159a75a4bc21c0c3975682ae97
dobbyRR <zxr1033@gmail.com>
2025-09-10 11:47:23 +0900

    [Feature] 기업 관리 페이지 채용공고 생성

--- frontend/src/views/company/CompanyShell.vue
+++ frontend/src/views/company/CompanyShell.vue
@@ -2,9 +2,6 @@
     <div>
         <header class="border-b">
         <CompanySwitcher :activeSlug="companySlug" />
-        <nav class="flex gap-4 p-3 text-sm">
-            <RouterLink :to="{ name:'CompanyDashboard', params:{ companySlug } }">대시보드</RouterLink>
-        </nav>
         </header>
         <RouterView :companySlug="companySlug" />
     </div>
