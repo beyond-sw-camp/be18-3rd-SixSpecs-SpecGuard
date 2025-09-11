0fc080ac5d73dd465bb088e1edc312cbca54aedc
dobbyRR <zxr1033@gmail.com>
2025-09-11 10:09:43 +0900

    [Feature] company shell 우측 여백 수정

--- frontend/src/views/company/CompanyShell.vue
+++ frontend/src/views/company/CompanyShell.vue
@@ -21,7 +21,7 @@
         </div>
         </header>
 
-        <div class="mx-auto max-w-10xl grid grid-cols-12 gap-6 px-4 sm:px-6 lg:px-8 py-6">
+        <div class="mx-auto max-w-10xl grid grid-cols-12 gap-6 pl-4 sm:pl-6 lg:pl-8 pr-0 py-6">
         <!-- Sidebar -->
         <transition name="fade-slide" mode="out-in">
             <!-- 펼친 상태 -->
@@ -131,8 +131,8 @@
         </transition>
         
         <main :class="sidebarOpen
-            ? 'col-span-12 sm:col-span-9 lg:col-span-7'
-            : 'col-span-12 lg:col-span-8'">
+            ? 'col-span-12 lg:col-span-10'
+            : 'col-span-12 lg:col-span-11'">
         <RouterView :companySlug="companySlug" />
         </main>
     </div>
