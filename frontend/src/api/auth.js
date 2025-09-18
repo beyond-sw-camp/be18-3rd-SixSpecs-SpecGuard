3fe4d7ba8c9cec5a2ede7abc0de3ca34667d42ad
dobbyRR <zxr1033@gmail.com>
2025-09-18 09:01:57 +0900

    no message

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -3,10 +3,10 @@ import api from "./axios";
 export const signup = (payload) => api.post("/auth/signup/company", payload);
 
 export const login = (email, password) =>
-  api.post(
+    api.post(
     "/auth/login",
     { email, password },                     // ← JSON 바디
     { withCredentials: true, headers: { "Content-Type": "application/json" } }
-  );
+    );
 
 export const logout = () => api.post("/auth/logout");
