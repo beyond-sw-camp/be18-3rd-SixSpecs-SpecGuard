d2906b50b3cedb74a799142b8c299040e8576b7d
viroovr <tjgusdnjs789@gmail.com>
2025-09-24 09:40:17 +0900

    fix 로그인 화면 대시보드로 가는거 수정

--- frontend/src/views/company/CompanyLogin.vue
+++ frontend/src/views/company/CompanyLogin.vue
@@ -6,11 +6,12 @@
         
     <!--메인 홈으로 돌아가~!-->
     <RouterLink 
-    :to="`/c/${companySlug}/dashboard`" 
-    class="text-2xl font-extrabold tracking-tight hover:text-amber-400 transition-colors"
-    >
-    SPECGUARD
-    </RouterLink>
+            :to="auth.isLoggedIn && companySlug && companySlug != null
+                ? `/c/${companySlug}/dashboard` 
+                : '/'"
+            class="text-2xl font-extrabold tracking-tight hover:text-amber-400 transition-colors">
+            SPECGUARD
+        </RouterLink>
 
 
             <span class="text-[11px] leading-none text-slate-300 mb-1">이력 검증 시스템</span>
